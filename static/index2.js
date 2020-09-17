985dc9cf0eb1f388659a3e11ad35d042909037e6
robe31 <jerobe.rob@gmail.com>
2020-09-17 15:00:22 -0500

    updated some ui functions, add sound to click event

--- static/index2.js
+++ static/index2.js
@@ -29,6 +29,9 @@ const TIME = (() => {
             let time = timer.currentTime;
 
             let minutes = ~~((time % 3600) / 60);
+            if(minutes < 10) {
+                minutes = `0${minutes}`
+            }
             let seconds = ~~time % 60;
             if(!(time % 60)) {
                 seconds = `0${seconds}`;
@@ -40,6 +43,9 @@ const TIME = (() => {
             timer.currentTime--;
 
             let minutes = ~~((timer.currentTime % 3600) / 60);
+            if(minutes < 10) {
+                minutes = `0${minutes}`
+            }
             let seconds = ~~timer.currentTime % 60;
 
             if(seconds < 10) {
@@ -68,19 +74,19 @@ const UI = (() => {
         tri: '#0EB567'
     }
 
-    const
-    pomodoro = document.querySelector(selectors.pomodoro),
-    shortBreak = document.querySelector(selectors.shortBreak),
-    longBreak = document.querySelector(selectors.longBreak),
-    startBtn = document.querySelector(selectors.startBtn),
-    stopBtn = document.querySelector(selectors.stopBtn),
-    main = document.querySelector(selectors.main),
-    nav = document.querySelector(selectors.nav);
-
+    const getUI = {
+        pomodoro: document.querySelector(selectors.pomodoro),
+        shortBreak: document.querySelector(selectors.shortBreak),
+        longBreak: document.querySelector(selectors.longBreak),
+        startBtn: document.querySelector(selectors.startBtn),
+        stopBtn: document.querySelector(selectors.stopBtn),
+        main: document.querySelector(selectors.main),
+        nav: document.querySelector(selectors.nav)
+    }
     
     return {
-        getSelectors: () => {
-            return selectors;
+        getElements: () => {
+            return getUI;
         },
         getColors: () => {
             return colors
@@ -91,103 +97,57 @@ const UI = (() => {
             `;
         },
         changeColors: (color) => {
-            main.style.background = color;
-            nav.style.background = color;
-            stopBtn.style.color = color;
-            startBtn.style.color = color;
+            getUI.main.style.background = color;
+            getUI.nav.style.background = color;
+            getUI.stopBtn.style.color = color;
+            getUI.startBtn.style.color = color;
         },
         addAndRemoveActive: (add, remove1, remove2) => {
             add.classList.add('active')
             remove1.classList.remove('active');
             remove2.classList.remove('active');
-        },
-        timer: {
-            set: null,
-    
-            start: () => {
-                console.log(log.currentSession)
-                timer.set = setInterval(() => {
-                    const time = TIME.getCurrentTime();
-                    if(time === 0){
-                        clearInterval(timer.set);
-                        timer.displayNotification();
-                    } else {
-                        UI.displayCountdown(TIME.countDown());
-                    }
-                }, 1000)
-                timer.startStopBtnPosition('none', 'inline')
-                askForNotification();
-            },
-    
-            stop: () => {
-                clearInterval(timer.set)
-                timer.startStopBtnPosition('inline', 'none')
-            },
-    
-            displayNotification: () => {
-                if(log.currentSession === 'pomodoro') {
-                    new Notification(`${log.currentSession} is over!`, { body: "Take your break"});
-                } else if(log.currentSession === 'short break') {
-                    new Notification(`${log.currentSession} is over!`, { body: "Get Back To Work!!!!"});
-                } else {
-                    new Notification(`${log.currentSession} is over!`, { body: "That Was Relaxing, Now Get Back To Work!!!!"});
-                }
-            }, 
-            startStopBtnPosition: (start, stop) => {
-                startBtn.style.display = start;
-                stopBtn.style.display = stop;
-            }
         }
     }
 })();
 
 const App = ((UI, TIME, Sessions) => {
     const 
-    selectors = UI.getSelectors(),
     colors = UI.getColors(),
-    log = Sessions.getLog();
+    log = Sessions.getLog(),
+    getUI = UI.getElements();
 
-    //Get App Element
-    const
-    pomodoro = document.querySelector(selectors.pomodoro),
-    shortBreak = document.querySelector(selectors.shortBreak),
-    longBreak = document.querySelector(selectors.longBreak),
-    startBtn = document.querySelector(selectors.startBtn),
-    stopBtn = document.querySelector(selectors.stopBtn);
-
-
-    let loadEvents = () => {
+    const loadEvents = () => {
         //pomodoro event
-        pomodoro.addEventListener('click', pomodoroTimer);
+        getUI.pomodoro.addEventListener('click', pomodoroTimer);
 
         //short break event
-        shortBreak.addEventListener('click', shortBreakTimer);
+        getUI.shortBreak.addEventListener('click', shortBreakTimer);
 
         //long break event
-        longBreak.addEventListener('click', longBreakTimer);
+        getUI.longBreak.addEventListener('click', longBreakTimer);
         
         //start event
-        startBtn.addEventListener('click', UI.timer.start);
+        getUI.startBtn.addEventListener('click', timer.start);
 
         //stop event
-        stopBtn.addEventListener('click', UI.timer.stop);
+        getUI.stopBtn.addEventListener('click', timer.stop);
     }
 
-    let pomodoroTimer = () => {
+    function pomodoroTimer() {
         log.currentSession = 'pomodoro';
         TIME.create(1500);
         UI.addAndRemoveActive(pomodoro, longBreak, shortBreak)
         UI.changeColors(colors.main)
         UI.displayCountdown(TIME.convert())
     }
-    let shortBreakTimer = () => {
+    function shortBreakTimer() {
         log.currentSession = 'short break';
         TIME.create(10);
-        UI.addAndRemoveActive(shortBreak, longBreak, pomodoro)
-        UI.changeColors(colors.sec)
-        UI.displayCountdown(TIME.convert())
+        UI.addAndRemoveActive(shortBreak, longBreak, pomodoro);
+        UI.changeColors(colors.sec);
+        UI.displayCountdown(TIME.convert());
     }
-    let longBreakTimer = () => {
+    function longBreakTimer() {
         log.currentSession = 'long break';
         TIME.create(900);
         UI.addAndRemoveActive(longBreak, shortBreak, pomodoro)
@@ -195,43 +155,54 @@ const App = ((UI, TIME, Sessions) => {
         UI.displayCountdown(TIME.convert())
     }
     // ----------------------------------------------------------------
-    // let timer = {
-    //     set: null,
+    const timer = {
+        set: null,
+        click: () => {
+            let click = new Audio('click.mp3');
+            click.play()
+        },
+        start: () => {
+            timer.click()
+            console.log(log.currentSession)
+            timer.set = setInterval(() => {
+                const time = TIME.getCurrentTime();
+                if(time === 0){
+                    clearInterval(timer.set);
+                    timer.displayNotification();
+                } else {
+                    UI.displayCountdown(TIME.countDown());
+                }
+            }, 1000)
+            timer.startStopBtnPositionAndStye('none', 'inline', true)
+            askForNotification();
+        },
 
-    //     start: () => {
-    //         console.log(log.currentSession)
-    //         timer.set = setInterval(() => {
-    //             const time = TIME.getCurrentTime();
-    //             if(time === 0){
-    //                 clearInterval(timer.set);
-    //                 timer.displayNotification();
-    //             } else {
-    //                 UI.displayCountdown(TIME.countDown());
-    //             }
-    //         }, 1000)
-    //         timer.startStopBtnPosition('none', 'inline')
-    //         askForNotification();
-    //     },
+        stop: () => {
+            timer.click()
+            clearInterval(timer.set)
+            timer.startStopBtnPositionAndStye('inline', 'none', false)
+            
+        },
 
-    //     stop: () => {
-    //         clearInterval(timer.set)
-    //         timer.startStopBtnPosition('inline', 'none')
-    //     },
-
-    //     displayNotification: () => {
-    //         if(log.currentSession === 'pomodoro') {
-    //             new Notification(`${log.currentSession} is over!`, { body: "Take your break"});
-    //         } else if(log.currentSession === 'short break') {
-    //             new Notification(`${log.currentSession} is over!`, { body: "Get Back To Work!!!!"});
-    //         } else {
-    //             new Notification(`${log.currentSession} is over!`, { body: "That Was Relaxing, Now Get Back To Work!!!!"});
-    //         }
-    //     }, 
-    //     startStopBtnPosition: (start, stop) => {
-    //         startBtn.style.display = start;
-    //         stopBtn.style.display = stop;
-    //     }
-    // }
+        displayNotification: () => {
+            if(log.currentSession === 'pomodoro') {
+                new Notification(`${log.currentSession} is over!`, { body: "Take your break"});
+            } else if(log.currentSession === 'short break') {
+                new Notification(`${log.currentSession} is over!`, { body: "Get Back To Work!!!!"});
+            } else {
+                new Notification(`${log.currentSession} is over!`, { body: "That Was Relaxing, Now Get Back To Work!!!!"});
+            }
+        }, 
+        startStopBtnPositionAndStye: (start, stop, x) => {
+            if(x) {
+                getUI.startBtn.style.borderBottom = "10px solid rgba(0,0,0,.1)";
+            } else {
+                getUI.stopBtn.style.borderBottom = "none";
+            }
+            getUI.startBtn.style.display = start;
+            getUI.stopBtn.style.display = stop;
+        }
+    }
 
     
     
@@ -239,8 +210,7 @@ const App = ((UI, TIME, Sessions) => {
         init: () => {
             // Display Page Load Timer
             pomodoroTimer()
-
-            UI.timer.startStopBtnPosition('inline', 'none');
+            timer.startStopBtnPositionAndStye('inline', 'none', true);
             loadEvents();
         }
     }
